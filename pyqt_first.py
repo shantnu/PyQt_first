d1f60e4f1e2c6d67633c5315fd0d9a9d4e16564e
Shantnu Tiwari <a@a.com>
2015-03-17 15:57:35 +0000

    first commit

---
+++ pyqt_first.py
@@ -0,0 +1,27 @@
+from __future__ import division
+import sys
+from PyQt4 import QtCore, QtGui, uic
+
+qtCreatorFile = "tax_calc.ui" # Enter file here.
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtGui.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.calc_tax_button.clicked.connect(self.CalculateTax)
+    
+    def CalculateTax(self):
+        price = int(self.price_box.toPlainText())
+        tax = (self.tax_rate.value())
+        total_price = price  + ((tax / 100) * price)
+        total_price_string = "The total price with tax is: " + str(total_price)
+        self.results_window.setText(total_price_string)
+        
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
