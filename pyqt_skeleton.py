9efd92c149340ad71307f28869f2e7b3eb4ae43c
Shantnu Tiwari <shantnu@Shantnus-MacBook-Air.local>
2021-08-26 15:21:37 +0100

    update for pyqt5

--- pyqt_skeleton.py
+++ pyqt_skeleton.py
@@ -1,18 +1,18 @@
 import sys
-from PyQt4 import QtCore, QtGui, uic
+from PyQt5 import QtWidgets, uic
 
-qtCreatorFile = "" # Enter file here.
+qtcreator_file  = "" # Enter file here.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-class MyApp(QtGui.QMainWindow, Ui_MainWindow):
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = MyApp()
     window.show()
     sys.exit(app.exec_())
